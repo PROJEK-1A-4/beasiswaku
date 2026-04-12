6fee75cbecd52f1dd6bf521a2bcb45fc60e76713
Richard <richardfadhilah120@gmail.com>
2026-04-12 14:39:36 +0700

    initial commit

---
+++ src/visualization/visualisasi.py
@@ -0,0 +1,10 @@
+"""
+visualisasi.py - Analytics and Charts Generation
+Owner: RICHARD
+Project: BeasiswaKu - Personal Scholarship Manager
+
+Tanggung jawab:
+- Generate Matplotlib pie charts & bar charts untuk Tab Tracker & Tab Statistik
+- Handle data aggregation functions dari crud.py
+- Return FigureCanvas (QWidget) agar bisa diembed langsung ke PyQt6 layout
+"""
